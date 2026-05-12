331e433673c4a85b316603e541fae7fdfdb426c3
acram <acram@example.com>
2026-05-11 21:37:17 -0500

    Initial commit: Express server with Travlr Getaways static pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
+var express = require("express");
+var path = require("path");
 var router = express.Router();
 
+function travlrPage(page) {
+  return function (req, res, next) {
+    res.sendFile(
+      path.join(__dirname, "..", "public", "images", "travlr", page),
+    );
+  };
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/", travlrPage("index.html"));
+router.get("/about", travlrPage("about.html"));
+router.get("/contact", travlrPage("contact.html"));
+router.get("/meals", travlrPage("meals.html"));
+router.get("/news", travlrPage("news.html"));
+router.get("/rooms", travlrPage("rooms.html"));
+router.get("/travel", travlrPage("travel.html"));
 
 module.exports = router;
